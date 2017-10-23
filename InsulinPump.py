70f8724a703646c395ff9275cadbb71000cc1336
Jack Chilby <jack.chilby@my.jcu.edu.au>
2017-10-23 17:02:22 +1000

    Added class and self.

--- InsulinPump.py
+++ InsulinPump.py
@@ -1,45 +1,52 @@
-current = 6
-previous = 5
-HIGH = 7
-MID = 5
-LOW = 3
+class Pump:
+    current = 0
+    previous = 5
 
-def main():
-    pass
+    HIGH = 7
+    MID = 5
+    LOW = 3
 
-def getRate():
-    pass
+    def main(self):
+        pass
 
-def sugarLow():
+    def getRate(self):
+        # I want this to get current reading from the server
+        pass
 
+    def sugarLow(self):
+        pass
 
-def calculate():
-    if getRate() > 0:                         # Checking if Sugar Level (SL) rising
-        if current > HIGH:
-            print('UNSAFE')
-        elif current > MID:
-            print('Safe, but high')
-        elif current > LOW:
-            print('Safe, but low')
-        elif current < LOW:
-            print('Not good ,eat!')
+    def calculate(self):
+        if self.getRate() > 0:                         # Checking if Sugar Level (SL) rising
+            if self.current > self.HIGH:
+                print('UNSAFE')
+            elif self.current > self.MID:
+                print('Safe, but high')
+            elif self.current > self.LOW:
+                print('Safe, but low')
+            elif self.current < self.LOW:
+                print('Not good ,eat!')
 
-    elif getRate() < 0:                         # Checking if Sugar Level (SL) falling
-        if current > HIGH:
-            print('UNSAFE')
-        elif current > MID:
-            print('Safe, but high')
-        elif current > LOW:
-            print('Safe, but low')
-        elif current < LOW:
-            print('Not good ,eat!')
+        elif self.getRate() < 0:                         # Checking if Sugar Level (SL) falling
+            if self.current > self.HIGH:
+                print('UNSAFE')
+            elif self.current > self.MID:
+                print('Safe, but high')
+            elif self.current > self.LOW:
+                print('Safe, but low')
+            elif self.current < self.LOW:
+                print('Not good ,eat!')
 
-    elif getRate() == 0:
-        if current > HIGH:
-            print('UNSAFE')
-        elif current > MID:
-            print('Safe, but high')
-        elif current > LOW:
-            print('Safe, but low')
-        elif current < LOW:
-            print('Not good ,eat!')
+        elif self.getRate() == 0:
+            if self.current > self.HIGH:
+                print('UNSAFE')
+            elif self.current > self.MID:
+                print('Safe, but high')
+            elif self.current > self.LOW:
+                print('Safe, but low')
+            elif self.current < self.LOW:
+                print('Not good ,eat!')
+
+"""
+Have calc call admin to administer insulin and take away from total + add to cumulative.
+"""
