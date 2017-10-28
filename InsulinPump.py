298f8bf7a1c92e4823d673b6d87b2c70a0157493
Jack Chilby <jack.chilby@my.jcu.edu.au>
2017-10-28 22:54:52 +1000

    Added fuinctions dealing with dosages
    Added functions to handle dosage and calculate how much insulin is left.

--- InsulinPump.py
+++ InsulinPump.py
@@ -1,52 +1,96 @@
 from getTestData import *
-
+from time import gmtime, strftime
 
 HIGH = 7
 LOW = 3
-
+MAX_DOSAGE = 10
 
 def main():
     count = 1                                       # This iterates through the data in CSV.
     current = 0
-    manual_mode = False
-    # while manual_mode is False:                   # Manual/Auto loop switch
+    reservoirCapacity = 10
+    manualMode = False
+    # while manualMode is False:                    # Manual/Auto loop switch
     for i in range(19):
-        print("Running startup Checks")
+        # print("Running startup Checks")
         checks(count)                               # startup checks
+        if checkReservoir(count):
+            newReservoir = True
+        else:
+            newReservoir = False
         previous = current
         current = getLevels(count)
         print("current=",current, "previous=", previous)
-        getZone(current, previous)
+        rate = getRate(current, previous)
+        if administer(rate, current):
+            if cumlativeDose():
+                reservoirLevel(rate, newReservoir)
+            else:
+                pass # Error for too much dose in a day.
         count += 1
+
+def cumlativeDose(rate):
+    now = strftime("%H:%M:%S", gmtime())
+    if str(now) == "00:00:00":
+        dailyDosage = 0
+    elif (dailyDosage + rate) > MAX_DOSAGE:
+        print("Maximum Daily Insulin will be exceeded")
+        return False
+    elif (dailyDosage + rate) <= MAX_DOSAGE:
+        dailyDosage += rate
+        return dailyDosage
+
 
 def getLevels(count):
     current = int(getData(count, 6))
     return current
 
-def getZone(current, previous):
-    if (int(round((current - previous), 0))) > 0:
+
+def getRate(current, previous):
+    rate = int(round((current - previous), 0))
+    return rate
+
+
+def administer(rate, current):
+    if rate > 0:
         if current > HIGH:
-            print('UNSAFE')
+            print('Blood Sugar levels high, ', rate, 'delivered.')
+            return True
+        elif LOW <= current <= HIGH:
+            print('Safe')
+            return False
+        elif current < LOW:
+            print("Your Blood Sugar Levels are low!")
+            return True
+
+    elif rate < 0:
+        if current > HIGH:
+            print('Blood Sugar levels high, ', rate, 'delivered.')
         elif LOW <= current <= HIGH:
             print('Safe')
         elif current < LOW:
             print("Your Blood Sugar Levels are low!")
 
-    elif (int(round((current - previous), 0))) < 0:
+    elif rate == 0:
         if current > HIGH:
-            print('UNSAFE')
+            print('Blood Sugar levels high, ', rate, 'delivered.')
         elif LOW <= current <= HIGH:
             print('Safe')
         elif current < LOW:
             print("Your Blood Sugar Levels are low!")
 
-    elif (int(round((current - previous), 0))) == 0:
-        if current > HIGH:
-            print('UNSAFE')
-        elif LOW <= current <= HIGH:
-            print('Safe')
-        elif current < LOW:
-            print("Your Blood Sugar Levels are low!")
+
+def reservoirLevel(rate, newReservoir):
+    print(rate, newReservoir)
+    if newReservoir:
+        level = 10
+    else:
+        level -= rate
+
+    if level <= 0:
+        print("Reservoir is Empty!")
+    else:
+        return level
 
 
 def checks(count):
@@ -57,8 +101,14 @@ def checks(count):
     reservoirStatus = getData(count, 5)
     statusArray = []
     statusArray.extend([sensorStatus, pumpStatus, deliveryStatus, needleStatus, reservoirStatus])
-    print(statusArray)
+    return statusArray
 
+def checkReservoir(count):
+    print(getData(count, 7))
+    if getData(count, 7) == 'TRUE':
+        return True
+    else:
+        return False
 
 main()
 
